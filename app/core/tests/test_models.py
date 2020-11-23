92fe1ca766b4744fa04cb356c54a480ae1762da2
Luana <fvd_ljun@uolinc.com>
2020-11-23 15:43:27 -0300

    Add admin model and tests

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,16 +1,56 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
+
+
+def sample_user(email='test@londoappdev.com', password='testpass'):
+    return get_user_model().objects.create_user(email, password)
+
 
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
-        """Test creating a new user with an email is successful"""
-        email = 'test@test.com'
-        password = 'Password123'
+        email = 'test@londoappdev.com'
+        password = 'test1234'
         user = get_user_model().objects.create_user(
-			email=email,
-			password=password
-		)
+            email=email,
+            password=password
+        )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        email = 'test@londoappdev.com'
+        user = get_user_model().objects.create_user(email, 'test1234')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test1234')
+
+    def test_create_new_super_user(self):
+        user = get_user_model().objects.create_superuser(
+            'test@londoappdev.com',
+            'test1234'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
+    def test_tag_string(self):
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+
+        self.assertEqual(str(tag), tag.name)
+
+    def test_ingredient_str(self):
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name='Cucumber'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
